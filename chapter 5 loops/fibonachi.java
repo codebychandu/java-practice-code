0d39f4e1e4a42ec452dc62dc4667b3de823bc37d
Chandu Kumar <chandukumar95079@gmail.com>
2024-04-08 23:19:06 +0530

    fibonachi correction

--- chapter 5 loops/fibonachi.java
+++ chapter 5 loops/fibonachi.java
@@ -11,18 +11,8 @@ public class fibonachi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
-        int a = sc.nextInt();
-
-        int b = 0;
-        int c = 1;
-        for (int i = 3; i <= a; i++) {
-            b = c;
-            System.out.println(b);
-        }
-    }
-
-    static void FibonacciExample1() {
-        int n1 = 0, n2 = 1, n3, i, count = 10;
+        int count = sc.nextInt();
+        int n1 = 0, n2 = 1, n3, i;
         System.out.print(n1 + " " + n2); 
         for (i = 2; i < count; ++i) { 
             n3 = n1 + n2;
@@ -30,6 +20,11 @@ public class fibonachi {
             n1 = n2;
             n2 = n3;
         }
+
+        sc.close();
+
         
     }
+
+    
 }
