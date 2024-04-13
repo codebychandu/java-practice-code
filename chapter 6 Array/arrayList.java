8eba95cb60bf67f966a6b8ad7c8b4a8fd1289acf
Chandu Kumar <chandukumar95079@gmail.com>
2024-04-13 13:47:22 +0530

    arraylist

--- chapter 6 Array/arrayList.java
+++ chapter 6 Array/arrayList.java
@@ -14,14 +14,42 @@ public class arrayList {
         System.out.println("First Element of ArrayList: " + list.get(0));
 
         // Get Size
-        System.out.println(array.length);
-        System.out.println(list.size());
+
+        System.out.println("Size of Array is: " + array.length);
+        System.out.println("Size of ArrayList is: " + list.size());
 
         //Add an Array
-        // You can't do that with an Array because array is immutable , but you can do in ArrayList    
-        list.add("e");
-        System.out.println(list.get(3));
+        // You can't do that with an Array because array is immutable , but you can do in ArrayList 
         
+        list.add("e");
+        System.out.println("But we can update ArrayList: " + list.get(3));
+
+        //Set an element 
+        array[0] = "f";
+        System.out.println("First Element of Array after change: " + array[0]);
+        list.set(1, "nandu");
+        System.out.println("First element of ArrayList after change: " + list.get(1));
+
+        // Remove an array
+        System.out.println("Sorry we can't do this in array");
+        list.remove("nandu");
+        System.out.println("First element of ArrayList after remove is: " + list.get(0));
+
+
+        // Print
+        // Array will not give the proper value, that's why developer prefered arraylist
+        System.out.println("Array is: " + array);
+        System.out.println("ArrayList is: " + list);
+        
+        // Iterate all the elements of ArrayList
+
+        // First way
+        // list.forEach(System.out::println);
+
+        // Second way
+        // for (String element : list) {
+        //     System.out.println(element);
+        // }
     }
     
 }
