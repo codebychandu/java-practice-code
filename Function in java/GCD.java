20546abaa531b4d59640d05c5530d2b1e2aef6a8
Chandu Kumar <chandukumar95079@gmail.com>
2024-04-30 22:59:28 +0530

    finally GCD program completed

--- Function in java/GCD.java
+++ Function in java/GCD.java
@@ -1,20 +1,26 @@
 
 
-// Write a function that calculates the Greatest Common Divisor of 2 numbers.
+// Greatest Common Divisor of 2 numbers.
 
 import java.util.*;
 public class GCD {
 
 
-    public static void GreaterNumber(int a, int b){
-        
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        int a = sc.nextInt();
+        System.out.print("Enter the first number: "); 
+        int a = sc.nextInt();  // 50
         System.out.print("Enter the second number: ");
-        int b = sc.nextInt();
+        int b = sc.nextInt();  // 60
+        int g = 0;
+        for (int i = 1; i <= a; i++) {
+            if (a%i == 0 && b%i == 0) {
+                g = i;
+            }
+        }
+        System.out.print("GCD is: " + g);  // ans 10
+
+
+        sc.close();
     }
 }
