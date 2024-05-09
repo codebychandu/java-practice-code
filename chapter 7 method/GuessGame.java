76c7b4fe71aa717d8759f52edf999efed040e5b6
Chandu Kumar <chandukumar95079@gmail.com>
2024-05-09 17:37:23 +0530

    added GuessGame.java

--- chapter 7 method/GuessGame.java
+++ chapter 7 method/GuessGame.java
@@ -9,9 +9,60 @@
   4. getter or setter of number of users.
  */
 
+import java.util.Random;
+import java.util.Scanner;
+
+class game{
+    public int number;
+    public int inputnumber;
+    public int noofguesses = 0;
+    
+
+    public int getnoofguesses(){
+        return noofguesses;
+    }
+    public void setnoofguess(int noofguesses){
+         this.noofguesses = noofguesses;
+    }
+
+    game(){
+        Random ran = new Random();
+        this.number = ran.nextInt(100);
+    }
+    void takeuserinput(){
+        System.out.println("Guess the number");
+        try (Scanner sc = new Scanner(System.in)) {
+            inputnumber = sc.nextInt();
+        }
+
+        // sc.close();
+    }
+    boolean iscorrectnumber(){
+        noofguesses++;
+        if(inputnumber == number){
+            System.out.printf("Yes you guessed it right , number was %d\nyou guessed it in %d attempts", number, noofguesses);
+            return true;
+        }
+        else if(inputnumber < number){
+            System.out.println("too low...");
+        }
+        else if(inputnumber > number){
+            System.out.println("too high...");
+        }
+        return false;
+        
+    }
+}
 
 public class GuessGame {
     public static void main(String[] args) {
+        game g = new game();
+        boolean b = false;
+        while (!b) {
+            g.takeuserinput();
+            b = g.iscorrectnumber();    
+            System.out.println(b);
+        }
         
     }
     
